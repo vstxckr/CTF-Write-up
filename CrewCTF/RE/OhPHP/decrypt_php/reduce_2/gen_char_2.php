ec58a277b8e846be1462e314e9261b65104d97df
vstxckr <manhdaoxuan2808@gmail.com>
2023-07-10 23:45:01 +0700

    Added reduce_2

---
+++ CrewCTF/RE/OhPHP/decrypt_php/reduce_2/gen_char_2.php
@@ -0,0 +1,34 @@
+<?php
+	$arr = array('[', ']', '(', ')', ':', '?', '.', ',', '^', '0');
+	
+	$len = sizeof($arr);
+	for ( $i = 0 ; $i < $len ; $i++ )
+	{
+		for ( $j = 0 ; $j < $len ; $j++ )
+		{
+			$t = $arr[$i]^$arr[$j];
+			if (ord($t) == 92 || ord($t) == 34)
+				printf("\ts = s.replace(\"('%s'^'%s')\", \"'\%s'\")\n", $arr[$i], $arr[$j], $t);
+			else if (ord($t) >= 32 && ord($t) <= 126)
+				printf("\ts = s.replace(\"('%s'^'%s')\", \"'%s'\")\n", $arr[$i], $arr[$j], $t);
+
+			for ( $k = 0 ; $k < $len ; $k++ )
+			{
+				$t = $arr[$i]^$arr[$j]^$arr[$k];
+				if (ord($t) == 92 || ord($t) == 34)
+					printf("\ts = s.replace(\"('%s'^'%s'^'%s')\", \"'\%s'\")\n", $arr[$i], $arr[$j], $arr[$k], $t);
+				else if (ord($t) >= 32 && ord($t) <= 126)
+					printf("\ts = s.replace(\"('%s'^'%s'^'%s')\", \"'%s'\")\n", $arr[$i], $arr[$j], $arr[$k], $t);
+
+				for ( $l = 0 ; $l < $len ; $l++ )
+				{
+					$t = $arr[$i]^$arr[$j]^$arr[$k]^$arr[$l];
+					if (ord($t) == 92 || ord($t) == 34)
+						printf("\ts = s.replace(\"('%s'^'%s'^'%s'^'%s')\", \"'\%s'\")\n", $arr[$i], $arr[$j], $arr[$k], $arr[$l], $t);
+					else if (ord($t) >= 32 && ord($t) <= 126)
+						printf("\ts = s.replace(\"('%s'^'%s'^'%s'^'%s')\", \"'%s'\")\n", $arr[$i], $arr[$j], $arr[$k], $arr[$l], $t);
+				}
+			}
+		}
+	}
+?>
